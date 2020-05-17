735491569988c6f2fdba0e9092f793d338ff3a93
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2020-05-17 17:48:00 -0300

    Add files via upload

---
+++ views/biologia/cores.js
@@ -0,0 +1,15 @@
+function mudaCorRed(){
+    document.getElementById('anotacoes').style.color = 'red';
+}
+function mudaCorBlue(){
+    document.getElementById('anotacoes').style.color = 'blue';
+
+}function mudaCorGreen(){
+    document.getElementById('anotacoes').style.color = 'green';
+
+}function mudaCorYellow(){
+    document.getElementById('anotacoes').style.color = 'yellow';
+
+}function mudaCorBlack(){
+    document.getElementById('anotacoes').style.color = 'black';
+}
